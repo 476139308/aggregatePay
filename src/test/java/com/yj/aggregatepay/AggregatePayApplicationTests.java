860d4546fdfba47b20d1d782985e239c6de72b27
yujin <476139308@qq.com>
2023-08-15 14:06:55 +0800

    Initial commit

---
+++ src/test/java/com/yj/aggregatepay/AggregatePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.aggregatepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatePayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
